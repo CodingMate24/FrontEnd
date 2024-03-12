26a12dd5b0aa8594bdb74369d12bf6fc9c642940
Seung Ho <shlee8689@gmail.com>
2024-03-12 14:47:45 +0900

    [추가] bootstrap, axios 라이브러리 추가

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
-<script>
-  export default {}
-</script>
-
 <template>
   <div id="app">
-    <router-view> </router-view>
+    <router-view/>
   </div>
 </template>
 
-<style>
+<script>
+export default {
+  name: 'App'
+}
+</script>
 
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+   color: #2c3e50;
+}
 </style>
--- src/findPass/findPass.vue
+++ src/findPass/findPass.vue
@@ -1,14 +1,26 @@
 <script>
 export default {};
+
+
+// 테스트 코드 - 추후 반영예쩡
+let formData = {"test" : "test"};
+
+const LoginService = function () {
+  axios.get('/api/user/login', formData, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => {
+    console.log("res.data :", res.data);
+  });
+};
 </script>
 
 <template>
-  <div class="row" id="findPass">
+  <div class="row border-1" id="findPass">
     <div class="col-xs-2 col-lg-4"/>
-    <div class="col-xs-8 col-lg-3">
-      <br/>
-      <br/>
-      <div class="row">
+    <div class="col-xs-8 col-lg-2">
+      <div id="lockLogo" class="row">
         <div class="icon_lock" />
       </div>
       <br/>
@@ -17,29 +29,35 @@ export default {};
         <p>가입할 때 입력한 이메일을 입력해 주세요.</p>
       </div>
       <div class="row d-grid gap-2">
-        <input id="email" class="form-control form-control-lg" type="text" placeholder="이메일을 입력해주세요." aria-label="default input example">
+        <input id="email" class="form-control" type="text" placeholder="이메일을 입력해주세요." aria-label="default input example">
+        <div id="passwordHelpBlock" class="form-text" style="color: red;">
+          해당 계정을 찾을 수 없습니다.
+        </div>
         <button id="sendMail" class="btn text-white btn-lg" style="background-color: #78B2F8;">임시비밀번호 발송</button>
       </div>
       <br/>
       <div class="row text-center">
-        <div class="col-5"><hr/></div>
-        <div class="col-2">또는</div>
-        <div class="col-5"><hr/></div>
+        <div class="col-4"><hr/></div>
+        <div class="col-4">또는</div>
+        <div class="col-4"><hr/></div>
       </div>
       <br/>
       <div class="row d-grid gap-2">
           <button id="signin" class="btn btn-outline-dark btn-lg">새 계정 만들기</button>
+          <br/>
           <button id="login" class="btn btn-outline-secondary btn-lg">로그인으로 돌아가기</button>
       </div>
     </div>
     <div class="col-xs-2 col-lg-4"/>
   </div>
-
 </template>
 
 <style>
   div {
     margin: 0 auto;
+  }
+  #lockLogo {
+    margin-top: 60px;
   }
   .icon_lock {
     background: url('image/icons.png') no-repeat -130px -0px;
