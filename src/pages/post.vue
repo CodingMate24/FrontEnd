599e127807ba4c8ef5c71fd151d08562193925e1
YoonjeongChoi <gkt0103@daum.net>
2024-10-13 18:38:33 +0900

    [추가] 로그인화면 추가

---
+++ src/pages/post.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+import LeftSection from "@/components/LeftSection.vue";
+</script>
+
+<template>
+  <div id="post">
+    <Header></Header>
+    <div id="main-section" class="row">
+      <div class="col-2">
+        <LeftSection></LeftSection>
+      </div>
+
+  </div>
+  </div>
+
+</template>
+
+<style>
+
+</style>
