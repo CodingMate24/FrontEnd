0d32d7ff1fcd5d57576c2540b09e8adcc7ba7df4
seungho <lsa6689@gmail.com>
2025-07-03 15:38:07 +0900

    [수정] 화면 레이아웃 수정

---
+++ src/components/LoginHeader.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="loginheader">
+    <header class="border-bottom">
+      <div class="big-logo" />
+    </header>
+  </div>
+</template>
+
+<script>
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {ref, onBeforeMount} from "vue";
+import Router from "@/router/index.js";
+
+export default {
+  computed: {
+  },
+  components: {FontAwesomeIcon},
+  setup(){
+    const routes = ref([]);
+
+    onBeforeMount(()=>{
+          routes.value = Router.options.routes;
+        }
+
+    )
+    return{routes};
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style>
+
+header {
+  padding: 15px 30px;
+  display: flex;
+  justify-content: space-between;
+  align-content: baseline;
+}
+
+.big-logo {
+  background: url('@/assets/logo_3.png') no-repeat -0px -0px;
+  width: 156px;
+  height: 35px;
+  margin: 0px;
+}
+
+.logout_btn {
+  float:right;
+  font-size: 15px;
+  text-decoration: none;
+  color: #000;
+  height: 35px;
+  margin: 0px;
+  padding-top: 5px;
+}
+#main-section {
+  height: calc(100vh - 172px);
+}
+
+#content-section {
+  height: calc(100vh - 172px);
+  border-left: #dee2e6 solid 1px;
+  --bs-gutter-x: 0;
+}
+</style>
--- src/pages/findpass.vue
+++ src/pages/findpass.vue
@@ -1,10 +1,10 @@
 <script>
 import axios from 'axios';
-import Header from "@/components/Header.vue";
+import LoginHeader from "@/components/LoginHeader.vue";
 import Footer from "@/components/Footer.vue";
 
 export default {
-  components: {Footer, Header}
+  components: {Footer, LoginHeader}
 };
 
 
@@ -23,65 +23,113 @@ const LoginService = new function () {
 </script>
 
 <template>
-  <Header/>
-  <div class="row" id="findPass">
-    <div class="col-xs-2 col-lg-4"/>
-    <div id="main-content" class="col-xs-8 col-lg-3 border">
-      <div id="lockLogo" class="row">
-        <div class="icon_lock" />
-      </div>
-      <br/>
-      <div class="row text-center">
-        <h4>로그인에 문제가 있나요?</h4>
-        <p>가입할 때 입력한 이메일을 입력해 주세요.</p>
-      </div>
-      <div class="row d-grid gap-2">
-        <input id="email" class="form-control-lg" type="text" placeholder="이메일을 입력해주세요." aria-label="default input example">
-        <div id="passwordHelpBlock" class="form-text visually-hidden" style="color: red;">
-          해당 계정을 찾을 수 없습니다.
+  <LoginHeader/>
+  <div id="findpass" class="row">
+    <div class="col-1">
+    </div>
+    <div class="col-10">
+      <div class="row div-center">
+        <div class="col-md-12 col-lg-12 signupLayer">
+          <div id="main-content">
+            <div class="row">
+              <div id="iconLogo" class="row">
+                <div class="icon_logo"></div>
+              </div>
+              <div id="lockLogo" class="row">
+                <div class="icon_lock"></div>
+              </div>
+              <br/>
+              <div class="row text-center">
+                <h4>로그인에 문제가 있나요?</h4>
+                <p>가입할 때 입력한 이메일을 입력해 주세요.</p>
+              </div>
+              <form>
+                <div class="col-12 d-grid gap-1 findpass-form">
+                  <div class="form-row">
+                    <div class="col-12 mb-3">
+                      <label for="chk_email" class="col-form-label">E-mail</label>
+                      <div class="input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="input-email">@</span>
+                        </div>
+                        <!-- 일치 : is-valid, 불일치: is-invalid -->
+                        <input type="text" class="form-control" id="chk_email" placeholder="이메일을 입력해주세요." required>
+                        <div class="invalid-feedback">
+                          가입되지 않은 이메일 입니다.
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <button id="'sendMail'" class="btn text-white btn-lg" style="background-color: #53bdeb; padding: 1px; font-size : 17px;font-weight: 700; height: 50px;">임시비밀번호 발송</button>
+                </div>
+              </form>
+            </div>
+            <div class="row center-line">
+              <div class="col-4"><hr/></div>
+              <div class="col-4">또는</div>
+              <div class="col-4"><hr/></div>
+            </div>
+            <div id="btn-groups" class="row d-grid gap-4">
+              <button id="signin" class="btn btn-outline-dark btn-md">새 계정 만들기</button>
+              <button id="login" class="btn btn-outline-secondary btn-md">로그인으로 돌아가기</button>
+            </div>
+          </div>
         </div>
-        <button id="sendMail" class="btn text-white btn-lg" style="background-color: #78B2F8;">임시비밀번호 발송</button>
-      </div>
-      <br/>
-      <div class="row text-center">
-        <div class="col-4"><hr/></div>
-        <div class="col-4">또는</div>
-        <div class="col-4"><hr/></div>
-      </div>
-      <br/>
-      <div id="orBtn" class="row d-grid gap-2">
-          <button id="signin" class="btn btn-outline-dark btn-lg">새 계정 만들기</button>
-          <br/>
-          <button id="login" class="btn btn-outline-secondary btn-lg">로그인으로 돌아가기</button>
       </div>
     </div>
-    <div class="col-xs-2 col-lg-4"/>
+    <div class="col-1">
+    </div>
   </div>
   <Footer/>
 </template>
 
 <style>
-  div {
-    margin: 0 auto;
+  #findpass{
+    height: 700px;
+    margin-top: 150px;
+    text-align: center;
+    display: flex;
+    justify-content: center;
   }
-  #findPass {
-    height: calc(100vh - 172px);
-  }
-  #lockLogo {
-    margin-top: 60px;
-  }
-  #orBtn {
-    margin-bottom: 60px;
-  }
-  #main-content {
-    padding: 0 90px;
-    height: 650px;
-    margin-top: 50px;
-    width: 480px;
+  #lockLogo{
+    display: flex;
+    justify-content: center;
+    padding-bottom: 20px;
   }
   .icon_lock {
     background: url('../assets/icons.png') no-repeat -130px -0px;
     width: 96px;
     height: 96px;
+  }
+  .div-center{
+    display: flex;
+    justify-content: center;
+  }
+  #main-content {
+    padding: 0 60px;
+  }
+  .signupLayer{
+    padding: 0;
+    max-width: 600px;
+    min-width: 500px;
+    justify-content: center;
+    align-items: center;
+  }
+  .center-line {
+    margin-top: 30px;
+    margin-bottom: 20px;
+  }
+  .findpass-form {
+    text-align: justify;
+  }
+
+  .icon_logo{
+    background: url('../assets/logo_1.png') no-repeat center;
+    width: 500px;
+    height: 96px;
+  }
+
+  #btn-groups{
+    margin-top: 30px;
   }
 </style>
